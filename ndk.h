71c76367c2090ad1e302a266e9b022efe56ab7d9
marcus <root@marcus.(none)>
2010-01-29 13:09:06 +0200

    Add version number, change config to using HTTP_INCS from CFLAGS

--- ndk.h
+++ ndk.h
@@ -12,18 +12,14 @@
 #include    <ngx_core.h>
 #include    <ngx_http.h>
 
-#include    <spl_devel.h>
+
+#define     ndk_version     2003
+#define     NDK_VERSION     "0.2.3"
 
 
-#if !(NDK_DEVEL)
 #if !(NDK)
 #error At least one module requires the Nginx Development Kit to be compiled with \
 the source (add --with-module=/path/to/devel/kit/src to configure command)
-#endif
-#else
-#ifndef NDK_ALL
-#define NDK_ALL 1
-#endif
 #endif
 
 
