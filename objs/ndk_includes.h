b5ec6dbba567c2c4257671c75857842159ef8fe7
root <root@debian6.localdomain>
2010-12-01 14:33:20 +0200

    Revert to old rewrite behaviour

---
+++ objs/ndk_includes.h
@@ -0,0 +1,57 @@
+// optional includes
+
+#if (NDK_COMPLEX_PATH)
+#include  <ndk_complex_path.h>
+#endif
+#if (NDK_COMPLEX_VALUE)
+#include  <ndk_complex_value.h>
+#endif
+#if (NDK_CONF_FILE)
+#include  <ndk_conf_file.h>
+#endif
+#if (NDK_ENCODING)
+#include  <ndk_encoding.h>
+#endif
+#if (NDK_HASH)
+#include  <ndk_hash.h>
+#endif
+#if (NDK_LOG)
+#include  <ndk_log.h>
+#endif
+#if (NDK_PATH)
+#include  <ndk_path.h>
+#endif
+#if (NDK_REGEX)
+#include  <ndk_regex.h>
+#endif
+#if (NDK_REWRITE)
+#include  <ndk_rewrite.h>
+#endif
+#if (NDK_SET_VAR)
+#include  <ndk_set_var.h>
+#endif
+#if (NDK_STRING)
+#include  <ndk_string.h>
+#endif
+#if (NDK_UPSTREAM_LIST)
+#include  <ndk_upstream_list.h>
+#endif
+#if (NDK_URI)
+#include  <ndk_uri.h>
+#endif
+
+
+// non-optional includes
+
+#include  <ndk_http_headers.h>
+
+
+// auto-generated headers
+
+#include  <ndk_array.h>
+#include  <ndk_palloc.h>
+#include  <ndk_conf_merge.h>
+#include  <ndk_conf_cmd_basic.h>
+#include  <ndk_conf_cmd_extra.h>
+
+
